1a2937b319de4c5ff8502881780efbce593ff8f1
Vitalii Kohut <vkohut@intelliarts.com>
2026-03-02 16:04:11 +0200

    perf: preconnect federation external domains

---
+++ src/app/[locale]/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="dns-prefetch" href="https://wnbfukraine.com.ua" />
+      <link rel="preconnect" href="https://wnbfukraine.com.ua" crossOrigin="" />
+      <link rel="dns-prefetch" href="https://worldnaturalbb.com" />
+      <link rel="preconnect" href="https://worldnaturalbb.com" crossOrigin="" />
+    </>
+  );
+}
